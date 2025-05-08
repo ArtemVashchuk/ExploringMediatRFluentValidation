e12c6abc643d5db48df88d8d26d4ecabbdba20d6
Artem Vashchuk <artem.vashchuk@gmail.com>
2025-05-08 11:46:44 +0300

    DEV-1: Add validation pipeline behavior

---
+++ gatherly/src/Gatherly.Application/Behaviors/ValidationPipelineBehavior.cs
@@ -0,0 +1,55 @@
+﻿using FluentValidation;
+using Gatherly.Domain.Shared;
+using MediatR;
+
+namespace Gatherly.Application.Behaviors;
+
+public class ValidationPipelineBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : Result
+{
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (!validators.Any())
+        {
+            return await next(cancellationToken);
+        }
+
+        Error[] errors = validators
+            .Select(validator => validator.Validate(request))
+            .SelectMany(validationResult => validationResult.Errors)
+            .Where(validationFailure => validationFailure is not null)
+            .Select(failure =>
+                new Error(failure.PropertyName, failure.ErrorMessage))
+            .Distinct()
+            .ToArray();
+
+        if (errors.Any())
+        {
+            return CreateValidationResult<TResponse>(errors);
+        }
+
+        return await next(cancellationToken);
+    }
+
+    private static TResult CreateValidationResult<TResult>(Error[] errors)
+        where TResult : Result
+    {
+        if (typeof(TResult) == typeof(Result))
+        {
+            return (ValidationResult.WithErrors(errors) as TResult)!;
+        }
+
+        object validationResult = typeof(ValidationResult<>)
+            .GetGenericTypeDefinition()
+            .MakeGenericType(typeof(TResult).GenericTypeArguments[0])
+            .GetMethod(nameof(ValidationResult.WithErrors))!
+            .Invoke(null, [errors])!;
+
+        return (validationResult as TResult)!;
+    }
+}
